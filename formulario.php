6ba370695c8b52de44b542ea1722a0cde60d1be4
yhimer <50939328+yhimer@users.noreply.github.com>
2019-06-01 01:20:05 -0500

    Add files via upload

---
+++ formulario.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+	<div class="container">
+		<div class="col-md-6">
+		    <div class="panel panel-primary">
+				<div class="panel-heading">	
+			<h2 class="text-center">INICIO DE SESION</h2>
+		</div>
+               	<div class="panel-body">
+               		<h1>Usuario: Yhimer Jara</h1>
+               		<a href="#">Agregar Articulo</a> <a href="#">Cerrar Sesion</a>
+               		<h1>Carrito Vacio</h1>
+               		
+               </div>
+
+        
+
+	</div>
+</div>
+</div>
+</div>
+    <script src="bootstrap/js/jquery-1.12.4.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ formulario2.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+    <div class="container">
+        <div class="col-md-6">
+            <div class="panel panel-primary">
+                <div class="panel-heading"> 
+            <h2 class="text-center">INICIO DE SESION</h2>
+        </div>
+        <form class="form-horizontal">
+            <div class="panel-body">
+                <label >Articulo</label>
+                <input type="text" name="txtarticulo" class="form-control">
+            </div>
+            <div class="panel-body">
+                <label >Precio</label>
+                <input type="text" name="txtarticulo" class="form-control">
+                
+            </div>
+            <div class="panel-body">
+                <label >Cantidad</label>
+                <input type="text" name="txtarticulo" class="form-control">
+                
+            </div>
+            <div class="panel-body">
+                <button type="submit" class="btn btn-success"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> Enviar</button>
+                
+            </div>
+
+
+        </form>
+
+
+
+
+
+
+           
